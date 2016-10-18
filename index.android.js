76ef50ad5b248913bcd971b139dfb3854c0a7f46
Rahim Rahman <rahim@r2integration.com>
2016-10-17 21:54:13 -0600

    Copied from a different project named SampleBingo and index.ios and index.android still using SampleBingo instead of Bingo

--- index.android.js
+++ index.android.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './src/app';
 
-AppRegistry.registerComponent('SampleBingo', () => App);
+AppRegistry.registerComponent('Bingo', () => App);
